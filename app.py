275292d86ccb073f3c15934b58d9d4b6d9ef3c74
AnnoyingRains <annoyingrain5@gmail.com>
2023-09-13 20:41:28 +1000

    Allow switching between comics from the comic page

--- app.py
+++ app.py
@@ -222,11 +222,62 @@ def comic(login_artist: Artist, comic_id):
         ).fetchone()[0]
     else:
         series = None
+
+    # find the next comic ID depending on the previous page
+    if request.args.get("via", default=None) is not None:
+        referrer = request.args.get("via")
+    else:
+        if request.referrer:
+            referrer = request.referrer.split("/")[3]
+        else:
+            referrer = None
+    match referrer:
+        case "series":
+            nextcomic = conn.execute(
+                "SELECT id FROM comics WHERE seriesid = ? AND id > ? LIMIT 1",
+                (comic["seriesid"], comic["id"]),
+            ).fetchone()
+            previouscomic = conn.execute(
+                "SELECT id FROM comics WHERE seriesid = ? AND id < ? ORDER BY id DESC LIMIT 1",
+                (comic["seriesid"], comic["id"]),
+            ).fetchone()
+        case "artists":
+            nextcomic = conn.execute(
+                "SELECT id FROM comics WHERE artistid = ? AND id > ? LIMIT 1",
+                (comic["artistid"], comic["id"]),
+            ).fetchone()
+            previouscomic = conn.execute(
+                "SELECT id FROM comics WHERE artistid = ? AND id < ? ORDER BY id DESC LIMIT 1",
+                (comic["artistid"], comic["id"]),
+            ).fetchone()
+        case "" | "index":
+            nextcomic = conn.execute(
+                "SELECT id FROM comics WHERE id > ? LIMIT 1",
+                (comic["id"],),
+            ).fetchone()
+            previouscomic = conn.execute(
+                "SELECT id FROM comics WHERE id < ? ORDER BY id DESC LIMIT 1",
+                (comic["id"],),
+            ).fetchone()
+            referrer = "index"
+        case _:
+            nextcomic = None
+            previouscomic = None
+
+    if nextcomic is not None:
+        nextcomic = nextcomic[0]
+
+    if previouscomic is not None:
+        previouscomic = previouscomic[0]
+
     return render_template(
         "comic.jinja",
         comic=comic,
         artist=artist[0],
         series=series,
+        nextcomic=nextcomic,
+        previouscomic=previouscomic,
+        referrer=referrer,
         login_artist=login_artist,
     )
 
